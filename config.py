03f912c344160a7e02ac4caac14af7b1f7a61615
MUFAZ VK <93363575+BXBotz2021@users.noreply.github.com>
2025-05-30 18:36:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ load_dotenv()
 
 class Config:
     # Telegram API
-    API_ID = int(os.getenv("API_ID", 0))
-    API_HASH = os.getenv("API_HASH", "")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+    API_ID = int(os.getenv("API_ID", 7813390))
+    API_HASH = os.getenv("API_HASH", "1faadd9cc60374bca1e88c2f44e3ee2f")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8168394361:AAHO-V1bqMutW-DZrSUW5d0wgMm6_MSOS88")
     
     # Redis
     REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
     REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-    REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
-    REDIS_DB = int(os.getenv("REDIS_DB", 0))
+    REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", "lpyhkguL4VmjkU8hyF8MHmbzmsDbE7RL")
+    REDIS_DB = int(os.getenv("REDIS_DB", "redis-16415.c228.us-central1-1.gce.redns.redis-cloud.com:16415"))
     
     # Instagram (optional)
     INSTA_USERNAME = os.getenv("INSTA_USERNAME", None)
